14fae1601be9a0b39bda746e2d75d3f5f578c822
min-gui <msj349911@gmail.com>
2022-10-17 21:12:55 +0900

    컨트롤러 최종

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+//서블릿 자동 등록
 @ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
