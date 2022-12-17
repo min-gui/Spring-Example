77d669fc6009c02ce872914e58d29704defb52bf
min-gui <msj349911@gmail.com>
2022-12-17 15:17:38 +0900

    add trace v2

---
+++ advanced/src/test/java/hello/advanced/trace/HelloTraceV1/HelloTraceV2Test.java
@@ -0,0 +1,36 @@
+package hello.advanced.trace.HelloTraceV1;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+
+public class HelloTraceV2Test {
+
+    @Test
+    public void begin_end() throws Exception {
+        HelloTraceV2 trace = new HelloTraceV2() ;
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.end(status2);
+        trace.end(status1);
+
+     }
+     
+     @Test
+     public void begin_exception() throws Exception {
+         //given
+         HelloTraceV2 trace = new HelloTraceV2() ;
+         TraceStatus status1 = trace.begin("hello1");
+         TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+         trace.exception(status2, new IllegalStateException());
+         trace.exception(status1, new IllegalStateException());
+
+         //then
+      
+      }
+         
+     
+
+
+
+}
